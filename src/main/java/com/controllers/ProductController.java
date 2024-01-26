f552db573a84dacaf2c6ea1d3a810d17ba4b2ab0
PhongVy <phongvy.work@gmail.com>
2024-01-26 22:35:43 +0700

    Update project
    Add jsp template

--- src/main/java/com/controllers/ProductController.java
+++ src/main/java/com/controllers/ProductController.java
@@ -39,7 +39,6 @@ public class ProductController extends HttpServlet {
             out.println("<h1>Servlet ProductController at " + request.getContextPath () + "</h1>");
             out.println("</body>");
             out.println("</html>");
-            out.println("Hello");
         }
     } 
 
